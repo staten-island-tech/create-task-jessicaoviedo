db21c9bc5c8081fd659d6f0914541286e4b4a563
jessicaoviedo <113190722+jessicaoviedo@users.noreply.github.com>
2023-02-09 10:14:27 -0500

    dyxdkdc bsld

---
+++ episodes.js
@@ -0,0 +1,320 @@
+const episodes = [
+    {
+        title: "Pilot",
+        episodenumber: 1,
+        season: 1,
+        description: "After Tori fills in for her sister in a big musical showcase, she's asked to enrol at an elite performing arts high school. But once there, she wonders if she'll ever fit in."
+    },
+    {
+        title: "The Bird Scene",
+        episodenumber: 2,
+        season: 1,
+        description: "Tori goes all out to master an iconic Hollywood Arts monologue in Sikowitz's class. Meanwhile, Andre and Robbie join a ballet class to meet girls with disastrous results.",
+    },
+    {
+        title: "Stage Fighting",
+        episodenumber: 3,
+        season: 1,
+        description: "Tori has to defend herself when Jade claims Tori hurt her in a stage fighting exercise, while Trina has to fend off Robbie when he thinks their stage kiss meant something."
+    },
+    {
+        title: "The Birthweek Song",
+        episodenumber: 4,
+        season: 1,
+        description: "Tori struggles to find the perfect birthday gift for Trina, then decides to write her a song as a present. Ungrateful Trina turns the song into cash by selling it."
+    },
+    {
+        title: "Jade Dumps Beck",
+        episodenumber: 5,
+        season: 1,
+        description: "Jade dumps Beck, then regrets it and recruits Tori to help get him back. Meanwhile, Trina puts on a one-woman show and tells Robbie to give it a good review, or else!"
+    },
+    {
+        title: "Tori the Zombie",
+        episodenumber: 6,
+        season: 1,
+        description: "Tori gets the lead in a high profile school musical, but freaks out when a stage make-up project goes wrong, and she's left looking like a zombie on opening night."
+    },
+    {
+        title: "Robarazzi",
+        episodenumber: 7,
+        season: 1,
+        description: "Tori suggests Robbie get videos of students' everyday lives to increase his blog traffic. When the camera is turned on Tori, she asks him to stop, but he doesn't."
+    },
+    {
+        title: "Video Chat",
+        episodenumber: 9,
+        season: 1,
+        description: "Tori's flight home is delayed, and she is left with no time to complete a school project. She tries to get the work done over a video chat with the team, but things keep getting in her way."
+    },
+    {
+        title: "Beck's Big Break",
+        episodenumber: 10, 
+        season: 1,
+        description: "Tori accidentally gets Beck fired from a small role in a movie, then must find a way to get him his job back. Meanwhile, Robbie has nightmares about Rex."
+    },
+    {
+        title: "The Great Ping Pong Scam",
+        episodenumber: 11,
+        season: 1,
+        description: "Tori discovers that her friends have been secretly going to a fancy restaurant instead of playing in an annual ping-pong tournament." 
+    },
+    {
+        title: "Cat's New Boyfriend",
+        episodenumber: 12,
+        season: 1,
+        description: "When Tori finds out that Cat is dating her ex-boyfriend, her jealousy could ruin their relationship. Trina uses fish to make everyone's feet super smooth."
+    },
+    {
+        title: "Freak the Freak Out",
+        episodenumber: 13,
+        season: 1,
+        description: "Cat and Jade get banned from a karaoke bar. The boys look after Trina after her wisdom teeth are removed, while Tori helps Jade and Cat get revenge."
+    },
+    {
+        title: "Rex Dies",
+        episodenumber: 14,
+        season: 1,
+        description: "As the kids prepare for a big play, Rex is accidentally sucked into a giant fan. Robbie is a wreck and it's up to Tori to save the day by saving Rex's life."
+    },
+    {
+        title: "The Diddly-Bops",
+        episodenumber: 15,
+        season: 1,
+        description: "Sikowitz recruits the gang to form a children's band for a friend's party. When a leaked video of the group becomes popular, Andre finds it hard to be taken seriously."
+    },
+    {
+        title: "Wok Star",
+        episodenumber: 16,
+        season: 1,
+        description: "Tori helps Jade find a place to perform a play that she wrote; Ms. Li threatens to cancel the show unless Jade casts her daughter in the play."
+    },
+    {
+        title: "Survival of the Hottest",
+        episodenumber: 17,
+        season: 1,
+        description: "A heat wave hits LA and Tori and the gang decide to go to Venice Beach to cool off. However, the gang - minus Cat who left to use the bathroom - become trapped in Beck's RV when another RV is parked right next to theirs. While they are trapped, Cat is busy partying with four hot guys, completely oblivious to her friends' predicament."
+    },
+    {
+        title: "The Wood",
+        episodenumber: 18,
+        season: 1,
+        description: "The kids are excited to hear that they'll be the stars of a new reality TV show. Meanwhile, Trina and Robbie must work at The Grub Truck when Festus is injured."
+    },
+    {
+        title: "Sleepover at Sikowitz's",
+        episodenumber: 19,
+        season: 1,
+        description: "Mr Sikowitz asks the gang to have a sleepover at his house, hoping to teach them a valuable lesson in method acting."
+    },
+    {
+        title: "A Film by Dale Squires",
+        episodenumber: 20,
+        season: 1,
+        description: "When a hot young filmmaker comes to Hollywood Arts, the kids are excited to work on his project. However, when the film is released, the kids want revenge." 
+    },
+    {
+        title:
+        episodenumber:
+        season:
+        description
+    },
+    {
+        title:
+        episodenumber:
+        season:
+        description
+    },
+    {
+        title:
+        episodenumber:
+        season:
+        description
+    },
+    {
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }{
+        title:
+        episodenumber:
+        season:
+        description
+    }
+]
